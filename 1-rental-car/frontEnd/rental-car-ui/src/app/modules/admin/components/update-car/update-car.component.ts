8824fd8cd3a119cd8afbb8f28207c672b58f52f5
NinoosMoshi <nalkaton@gmail.com>
2023-12-11 09:22:49 -0600

    update car

--- 1-rental-car/frontEnd/rental-car-ui/src/app/modules/admin/components/update-car/update-car.component.ts
+++ 1-rental-car/frontEnd/rental-car-ui/src/app/modules/admin/components/update-car/update-car.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { AdminService } from '../../services/admin.service';
@@ -12,8 +12,16 @@ import { AdminService } from '../../services/admin.service';
 export class UpdateCarComponent {
 
    isSpinning:boolean = false;
+   carForm!:FormGroup;
+   existingImage:string | null = null;
+   listOfOption: Array<{label:string; value:string}> = [];
+   listOfBrands = ['BMW','AUDI','TESLA','TOYOTA','HONDA'];
+   listOfType = ['Petrol','Hybrid', 'Diesel', 'Electric'];
+   listOfColor = ['Red', 'White', 'Blue', 'Black'];
+   listOfTransmission = ['Manual', 'Automatic'];
 
    carId:number = this.activatedRoute.snapshot.params['id'];
+
 
   constructor(private fb:FormBuilder,
     private adminService:AdminService,
@@ -25,22 +33,39 @@ export class UpdateCarComponent {
 
 
 ngOnInit(){
+  this.carForm = this.fb.group({
+    brand:[null, Validators.required],
+    name:[null, Validators.required],
+    type:[null, Validators.required],
+    transmission:[null, Validators.required],
+    color:[null, Validators.required],
+    date:[null, Validators.required],
+    price:[null, Validators.required],
+    description:[null, Validators.required]
+  })
   this.getCarById();
 }
 
 
 getCarById(){
+  this.isSpinning = true
    this.adminService.getCarById(this.carId).subscribe({
     next:res =>{
       this.isSpinning = false
       this.message.success("Get Car successfully", {nzDuration:5000})
-      console.log(res)
+      const carDTO = res;
+      this.existingImage = 'data:image/jpeg;base64,' + res.returnImage;
+      this.carForm.patchValue(carDTO);
+
     },
     error:err =>{
       this.message.error("Error while getting car", {nzDuration:5000})
     }
    })
 }
+
+
+
 
 
 }
